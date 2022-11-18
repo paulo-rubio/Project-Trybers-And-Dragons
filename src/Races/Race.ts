03f727d0aea1119c5c897e32cab82326390e31f8
Rubio <paulocezarrubio@gmail.com>
2022-11-18 15:13:23 -0300

    lint

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -10,23 +10,22 @@ export default abstract class Race {
   private _name: string;
   private _dexterity: number;
 
-  constructor(name: string , dexterity: number){
-    this._name = name
-    this._dexterity = dexterity
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
   }
 
-  get name(): string{
+  get name(): string {
     return this._name;
   }
 
-  get dexterity(): number{
+  get dexterity(): number {
     return this._dexterity;
   }
 
   static createdRacesInstances(): number {
-      throw new Error ('Not implemented');
-  }  
-  
+    throw new Error('Not implemented');
+  }
+
   abstract get maxLifePoints(): number;
-  
 }
