f27b6faf54b626e7e44cc16a5a2346dcbf4f0543
Rubio <paulocezarrubio@gmail.com>
2022-11-21 14:46:05 -0300

    req10-CriandoPvP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,20 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  _player1: Fighter;
+  _player2: Fighter;
+  constructor(player1: Fighter, player2: Fighter) {
+    super(player1);
+    this._player1 = player1;
+    this._player2 = player2;
+  }
+
+  fight(): number {
+    if (this.player.lifePoints > 0 && this._player2.lifePoints > 0) {
+      this._player1.attack(this._player2);
+      this._player2.attack(this._player1);
+    }
+    return super.fight();
+  }
+}
