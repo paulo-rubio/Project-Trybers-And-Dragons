274abcc5abfef5375f270445e59a390a9a3bdd2c
Paulo Rubio <102248463+paulo-rubio@users.noreply.github.com>
2022-12-02 17:12:58 -0300

    Update in lifepoints  in monster

--- src/Monster.ts
+++ src/Monster.ts
@@ -16,10 +16,8 @@ export default class Monster implements SimpleFighter {
 
   receiveDamage(attackPoints: number): number {
     const damage = this._lifePoints - attackPoints;
-    if (damage > 0) {
-      this._lifePoints -= damage;
-    }
-    if (this.lifePoints <= 0) {
+    
+    if (this._lifePoints <= 0) {
       this._lifePoints = -1;
     }
     return this._lifePoints;
